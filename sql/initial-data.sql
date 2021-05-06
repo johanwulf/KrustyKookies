3751907111f9adfdb33c55966e28c61df11593ea
johanwulf <johanwulf98@gmail.com>
2021-05-06 09:17:52 +0200

    started initial

---
+++ sql/initial-data.sql
@@ -0,0 +1,11 @@
+INSERT INTO Customers(name, address) VALUES
+    ('Finkakor AB', 'Helsingborg'),
+    ('Småbröd AB', 'Malmö'),
+    ('Kaffebröd AB', 'Landskrona'),
+    ('Bjudkakor AB', 'Ystad'),
+    ('Kalaskakor AB', 'Trelleborg'),
+    ('Partykakor AB', 'Kristianstad'),
+    ('Gästkakor AB', 'Hässleholm'),
+    ('Skånekakor AB', 'Perstorp')
+;
+
